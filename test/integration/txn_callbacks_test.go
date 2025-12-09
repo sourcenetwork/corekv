f7a73094dbe9d754cefe1196a658589c56c94a40
Keenan Nemetz <keenan.nemetz@gmail.com>
2025-12-09 14:33:37 -0800

    improve tests

--- test/integration/txn_callbacks_test.go
+++ test/integration/txn_callbacks_test.go
@@ -9,14 +9,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTxnCommitSuccessCallbacks(t *testing.T) {
+func TestTxnCommit_WithSuccessCallback(t *testing.T) {
 	var callbackCount atomic.Uint64
 	test := &Test{
 		Actions: []action.Action{
 			action.NewTxn(),
 			action.OnSuccess(func() { callbackCount.Add(1) }),
-			action.OnDiscard(func() { callbackCount.Add(1) }),
-			action.OnError(func() { callbackCount.Add(1) }),
+			action.OnDiscard(func() { callbackCount.Add(3) }),
+			action.OnError(func() { callbackCount.Add(5) }),
 			action.Commit(),
 		},
 	}
@@ -25,30 +25,47 @@ func TestTxnCommitSuccessCallbacks(t *testing.T) {
 	assert.Equal(t, uint64(1), callbackCount.Load())
 }
 
-func TestTxnDiscardCallbacks(t *testing.T) {
+func TestTxnDiscard_WithDiscardCallback(t *testing.T) {
 	var callbackCount atomic.Uint64
 	test := &Test{
 		Actions: []action.Action{
 			action.NewTxn(),
 			action.OnSuccess(func() { callbackCount.Add(1) }),
-			action.OnDiscard(func() { callbackCount.Add(1) }),
-			action.OnError(func() { callbackCount.Add(1) }),
+			action.OnDiscard(func() { callbackCount.Add(3) }),
+			action.OnError(func() { callbackCount.Add(5) }),
 			action.Discard(),
 		},
 	}
 
 	test.Execute(t)
-	assert.Equal(t, uint64(1), callbackCount.Load())
+	assert.Equal(t, uint64(3), callbackCount.Load())
 }
 
-func TestTxnCommitErrorCallbacks(t *testing.T) {
+func TestTxnCommitThenDiscard_WithSuccessAndDiscardCallback(t *testing.T) {
 	var callbackCount atomic.Uint64
 	test := &Test{
 		Actions: []action.Action{
 			action.NewTxn(),
 			action.OnSuccess(func() { callbackCount.Add(1) }),
-			action.OnDiscard(func() { callbackCount.Add(1) }),
-			action.OnError(func() { callbackCount.Add(1) }),
+			action.OnDiscard(func() { callbackCount.Add(3) }),
+			action.OnError(func() { callbackCount.Add(5) }),
+			action.Commit(),
+			action.Discard(),
+		},
+	}
+
+	test.Execute(t)
+	assert.Equal(t, uint64(4), callbackCount.Load())
+}
+
+func TestTxnCommit_WithErrorCallback(t *testing.T) {
+	var callbackCount atomic.Uint64
+	test := &Test{
+		Actions: []action.Action{
+			action.NewTxn(),
+			action.OnSuccess(func() { callbackCount.Add(1) }),
+			action.OnDiscard(func() { callbackCount.Add(3) }),
+			action.OnError(func() { callbackCount.Add(5) }),
 			action.WithTxn(action.Set([]byte("k1"), []byte("v1"))),
 			action.Close(),
 			action.CommitE(corekv.ErrDBClosed.Error()),
@@ -56,5 +73,24 @@ func TestTxnCommitErrorCallbacks(t *testing.T) {
 	}
 
 	test.Execute(t)
-	assert.Equal(t, uint64(1), callbackCount.Load())
+	assert.Equal(t, uint64(5), callbackCount.Load())
+}
+
+func TestTxnCommitThenDiscard_WithDiscardAndErrorCallback(t *testing.T) {
+	var callbackCount atomic.Uint64
+	test := &Test{
+		Actions: []action.Action{
+			action.NewTxn(),
+			action.OnSuccess(func() { callbackCount.Add(1) }),
+			action.OnDiscard(func() { callbackCount.Add(3) }),
+			action.OnError(func() { callbackCount.Add(5) }),
+			action.WithTxn(action.Set([]byte("k1"), []byte("v1"))),
+			action.Close(),
+			action.CommitE(corekv.ErrDBClosed.Error()),
+			action.Discard(),
+		},
+	}
+
+	test.Execute(t)
+	assert.Equal(t, uint64(8), callbackCount.Load())
 }
